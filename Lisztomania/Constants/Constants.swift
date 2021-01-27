ef92aecc5de4e570148273fa8eb04977be3c7298
Jaime Jazareno III <jaimejazarenoiii@Jaimes-MacBook-Pro.local>
2021-01-27 19:57:46 +0800

    Create constants

---
+++ Lisztomania/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Lisztomania
+//
+//  Created by Jaime Jazareno III on 1/21/21.
+//
+
+import Foundation
+
+enum Constants {
+    static let apiUrl = "https://api.spotify.com/v1"
+}
